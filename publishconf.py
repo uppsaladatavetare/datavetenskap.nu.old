1dee3d532ab088274bf3ac896078d05edaef07d0
Krzysztof Jagiello <me@kjagiello.com>
2017-03-14 09:36:22 +0100

    Serve the site via GitHub Pages

--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://www.datavetenskap.nu'
+SITEURL = 'https://dv.teambacon.se'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
