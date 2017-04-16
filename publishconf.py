19aebf5a5b5435e3113fa0d1cf491997887d5670
Krzysztof Jagiełło <me@kjagiello.com>
2017-04-16 21:24:04 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://dv.teambacon.se'
+SITEURL = 'http://www.datavetenskap.nu'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
